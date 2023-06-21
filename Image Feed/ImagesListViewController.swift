67a4f94690676b7d8d1de83a138c1edde3b5a9af
nnkmk <mailtonotoce@gmail.com>
2023-06-21 19:40:00 +0400

    <test>

---
+++ Image Feed/ImagesListViewController.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+class ImagesListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet private var tableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
